d99d78b182f81779208875aeb423228ef54f0b17
leiweibo <leiweibo@gmail.com>
2021-07-02 18:54:10 +0800

    add calculate the new hold share by monthly

--- database/models/NorthSecurity.js
+++ database/models/NorthSecurity.js
@@ -31,5 +31,4 @@ const northSecurity = db.define(
   },
 );
 
-
 module.exports = { northSecurity };
--- routers/northreport.js
+++ routers/northreport.js
@@ -1,8 +1,10 @@
 const router = require('koa-router')();
-const { Op } = require('sequelize');
+const { Op, Sequelize } = require('sequelize');
 
 const dayjs = require('dayjs');
 const { northHoldingReports } = require('../database/models/NorthHoldingReport');
+const { northHolding } = require('../database/models/NorthHolding');
+const { northSecurity } = require('../database/models/NorthSecurity');
 
 router.get('/:securityCode', async (ctx) => {
   const params = ctx.request.query;
@@ -39,6 +41,64 @@ router.get('/:securityCode', async (ctx) => {
       rows,
       total: count,
     },
+  };
+});
+
+const getHoldingGroupByMonth = async (month, fromScratch) => {
+  const startDate = fromScratch ? dayjs().set('month', 10).set('year', 2020).set('date', 1) : dayjs().set('month', month - 1).set('date', 1);
+  const endDate = dayjs().set('month', month).set('date', 1);
+  const rows = await northHolding.findAll({
+    attributes: [
+      'security_ccass_code',
+      [Sequelize.fn('sum', Sequelize.col('holding_amt')), 'total_holding_amount'],
+    ],
+    where: {
+      trade_date: {
+        [Op.and]: {
+          [Op.gte]: startDate.format('YYYY-MM-DD'),
+          [Op.lt]: endDate.format('YYYY-MM-DD'),
+        },
+      },
+    },
+    group: 'security_ccass_code',
+  });
+  return rows;
+};
+
+router.get('/monthly/diff', async (ctx) => {
+  const params = ctx.request.query;
+  const month = params.m;
+  const curMonthData = await getHoldingGroupByMonth(month);
+  const previousData = await getHoldingGroupByMonth(month - 1);
+
+  const diffResult = curMonthData.filter((v) => {
+    const exists = previousData.find((e) => e.security_ccass_code === v.security_ccass_code);
+    return !exists;
+  });
+  const targetList = diffResult.map((r) => r.security_ccass_code);
+  const northSecurityList = await northSecurity.findAll({
+    where: {
+      security_ccass_code: {
+        [Op.in]: targetList,
+      },
+    },
+  });
+
+  const finalResult = diffResult.map((r) => {
+    const tmp = northSecurityList.find((e) => e.security_ccass_code === r.security_ccass_code);
+    return {
+      totalHoldAmt: r.total_holding_amount,
+      securityCCassCode: r.security_ccass_code,
+      securityCode: tmp.security_code,
+      securityName: tmp.security_name,
+    };
+  });
+
+  ctx.body = {
+    succcess: true,
+    msg: 'get data success',
+    result: finalResult,
+    count: finalResult.length,
   };
 });
 
