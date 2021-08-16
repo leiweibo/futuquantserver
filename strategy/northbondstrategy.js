a31da5b4838c6a220f91622f6474a39f334aa3aa
leiweibo <leiweibo@gmail.com>
2021-08-16 23:23:22 +0800

    implementation the strategy 1

--- strategy/northbondstrategy.js
+++ strategy/northbondstrategy.js
@@ -16,22 +16,23 @@ const start = async () => {
     having: Sequelize.literal('security_mkt LIKE "% Northbound"'),
   });
   const tmpResult = rows.map((r) => r.dataValues);
-  const finalResult = new Map();
+  const norhtbondResult = new Map();
   tmpResult.forEach((result) => {
     const targetDate = dayjs(result.trade_date).format('YYYY-MM-DD');
     if (result.net_income) {
-      if (finalResult.has(targetDate)) {
-        const newVal = Number(finalResult.get(targetDate) + Number(result.net_income)).toFixed(2);
-        finalResult.set(targetDate, newVal);
+      if (norhtbondResult.has(targetDate)) {
+        const newVal = Number(norhtbondResult.get(targetDate)
+         + Number(result.net_income)).toFixed(2);
+        norhtbondResult.set(targetDate, newVal);
       } else {
-        finalResult.set(targetDate, parseFloat(result.net_income));
+        norhtbondResult.set(targetDate, parseFloat(result.net_income));
       }
     }
   });
-  const dateArray = Array.from(finalResult.keys());
+  const dateArray = Array.from(norhtbondResult.keys());
   const endDate = dateArray.slice(-1)[0];
   const startDate = dateArray[0];
-  const etf50Klines = await xueqiuClient('SH510050', startDate, endDate);
+  const etf50Klines = await xueqiuClient('SH510500', startDate, endDate);
   const klineMap = new Map();
   etf50Klines.data.item.forEach((kline) => {
     const date = dayjs(kline[0]).format('YYYY-MM-DD');
@@ -43,7 +44,64 @@ const start = async () => {
     });
     return 0;
   });
-  console.log(klineMap);
+
+  // 起始资金 10,000,000.00，达到条件开始执行买卖操作
+  let balance = 100000;
+  const operationRatio = 1;
+  let holdingAmt = 0;
+  const operationList = [];
+  const targetTradeResult = [...norhtbondResult].filter((r) => (r[1] >= 5000 || r[1] <= -5000));
+  targetTradeResult.forEach((t) => {
+    const kline = klineMap.get(t[0]);
+    if (t[1] > 0) {
+      // 买入操作：当天收盘价买入
+      let buyAmt = (balance * operationRatio) / kline.close;
+      buyAmt -= buyAmt % 100;
+      balance -= buyAmt * kline.close;
+      if (buyAmt > 0) {
+        holdingAmt += buyAmt;
+        operationList.push({
+          date: t[0],
+          prices: kline,
+          operationAmount: buyAmt,
+          holdingAmount: holdingAmt,
+          finalBalance: balance,
+        });
+      }
+    } else {
+      console.log('');
+      // 卖出操作
+      if (holdingAmt > 0 && holdingAmt * operationRatio <= 100) {
+        balance += holdingAmt * kline.close;
+        holdingAmt -= holdingAmt;
+        operationList.push({
+          date: t[0],
+          prices: kline,
+          operationAmount: -holdingAmt,
+          holdingAmount: holdingAmt,
+          finalBalance: balance,
+        });
+      } else if (holdingAmt > 0) {
+        const sellAmt = ((holdingAmt * operationRatio) - ((holdingAmt * operationRatio) % 100));
+        balance += (sellAmt * kline.close);
+        holdingAmt -= sellAmt;
+        operationList.push({
+          date: t[0],
+          prices: kline,
+          operationAmount: -sellAmt,
+          holdingAmount: holdingAmt,
+          finalBalance: balance,
+        });
+      }
+    }
+  });
+  // console.log(operationList);
+  const latestKline = etf50Klines.data.item.slice(-1)[0];
+  const ratio = (holdingAmt * latestKline[5] + balance - 100000) / 100000;
+  console.log(operationList);
+  console.log(`holding amount is: ${holdingAmt}`);
+  console.log(`balance is: ${balance.toFixed(2)}`);
+  console.log(`收益率为:${ratio}`);
 };
 
 (async () => {
