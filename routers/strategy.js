34fe27ed99667c12d66bf568d51cdd4d52f2f37b
leiweibo <leiweibo@gmail.com>
2021-08-23 23:58:46 +0800

    tmp commit

--- routers/strategy.js
+++ routers/strategy.js
@@ -4,6 +4,7 @@ const { strategy1 } = require('../strategy/northbondstrategy');
 
 router.get('/1', async (ctx) => {
   const params = ctx.request.query;
+  const nDays = params.ndays || 1;
   const securityCode = params.code || 'SH510500';
   const initBalance = Number(params.init || 100000);
   const buyRatio = Number(params.buyRatio || 1);
@@ -11,7 +12,7 @@ router.get('/1', async (ctx) => {
   ctx.body = {
     succcess: true,
     msg: 'execute strategy success.',
-    data: await strategy1(securityCode, initBalance, buyRatio, sellRatio),
+    data: await strategy1(securityCode, nDays, initBalance, buyRatio, sellRatio),
   };
 });
 
--- strategy/northbondstrategy.js
+++ strategy/northbondstrategy.js
@@ -4,14 +4,14 @@ const { northTransactionDetail } = require('../database/models/NorthTransactionD
 const { xueqiuClient } = require('../providers/xueqiu/xueqiuclient');
 
 /**
- * 单日北向资金净流出超过50亿元则以当日收盘价卖出，流入超过50亿元则以当日收盘价买入。
+ * 近三日北向资金累计流出超过50亿元则以当日收盘价卖出，累计流入超过50亿元则以当日收盘价买入。
  * @param {*} securityCode 对比的股票代码
  * @param {*} initBalance 起始资金
  * @param {*} buyRatio 购买金额比例
  * @param {*} sellRatio 卖出股票比例
  * @returns 收益率列表，包含当天的收盘价
  */
-const strategy1 = async (securityCode, initBalance, buyRatio, sellRatio) => {
+const strategy1 = async (securityCode, nDays, initBalance, buyRatio, sellRatio) => {
   const rows = await northTransactionDetail.findAll({
     attributes: [
       'trade_date',
@@ -52,15 +52,24 @@ const strategy1 = async (securityCode, initBalance, buyRatio, sellRatio) => {
     });
     return 0;
   });
-
+  const nDaysMap = new Map();
+  const valueArray = Array.from(norhtbondResult.values()).reverse();
+  valueArray.forEach((val, index) => {
+    let nDaysTotalResult = 0;
+    for (let i = 1; i < nDays; i++) {
+      nDaysTotalResult += Number((((index + i) <= valueArray.length - 1) ? valueArray[index + i] : '0.0'));
+    }
+    const result = Number(val) + Number(nDaysTotalResult);
+    nDaysMap.set(dateArray[valueArray.length - index - 1], result.toFixed(2));
+    return result;
+  });
+  const targetTradeResult = [...nDaysMap].filter((r) => (r[1] >= 5000 || r[1] <= -5000));
   // 起始资金 10,000,000.00，达到条件开始执行买卖操作
   let balance = initBalance;
   let holdingAmt = 0;
   // the operation list showed for test purpose.
   const operationList = [];
-  // the profit list.
   const profitList = [];
-  const targetTradeResult = [...norhtbondResult].filter((r) => (r[1] >= 5000 || r[1] <= -5000));
   targetTradeResult.forEach((t) => {
     const kline = klineMap.get(t[0]);
     let operateAmt = 0;
@@ -103,10 +112,10 @@ const strategy1 = async (securityCode, initBalance, buyRatio, sellRatio) => {
         northBound: t[1],
       });
     }
-
     profitList.push({
       date: t[0],
       northBounds: t[1],
+      prices: kline.close,
       operationAmount: operateAmt,
       holdingAmount: holdingAmt,
       finalBalance: balance,
@@ -135,7 +144,7 @@ const strategy1 = async (securityCode, initBalance, buyRatio, sellRatio) => {
     } else {
       finalProfitList.push({
         date,
-        northBounds: 0, // 这个数据没有去获取
+        northBounds: norhtbondResult.get(date), // 这个数据没有去获取
         operationAmount: 0,
         holdingAmount: 0,
         close: klineMap.get(date).close,
@@ -147,64 +156,8 @@ const strategy1 = async (securityCode, initBalance, buyRatio, sellRatio) => {
   return finalProfitList;
 };
 
-/**
- * 近三日北向资金累计流出超过50亿元则以当日收盘价卖出，累计流入超过50亿元则以当日收盘价买入。
- */
-const strategy2 = async () => {
-  const rows = await northTransactionDetail.findAll({
-    attributes: [
-      'trade_date',
-      'security_mkt',
-      [Sequelize.fn('sum', Sequelize.literal('(buy_trades - sell_trades)')), 'net_income'],
-    ],
-    group: [
-      ['trade_date'], ['security_mkt'],
-    ],
-    having: Sequelize.literal('security_mkt LIKE "% Northbound"'),
-  });
-  const tmpResult = rows.map((r) => r.dataValues);
-  const norhtbondResult = new Map();
-  tmpResult.forEach((result) => {
-    const targetDate = dayjs(result.trade_date).format('YYYY-MM-DD');
-    if (result.net_income) {
-      if (norhtbondResult.has(targetDate)) {
-        const newVal = Number(norhtbondResult.get(targetDate)
-         + Number(result.net_income)).toFixed(2);
-        norhtbondResult.set(targetDate, newVal);
-      } else {
-        norhtbondResult.set(targetDate, parseFloat(result.net_income));
-      }
-    }
-  });
-  const dateArray = Array.from(norhtbondResult.keys());
-  const endDate = dateArray.slice(-1)[0];
-  const startDate = dateArray[0];
-  const etf50Klines = await xueqiuClient('SH510500', startDate, endDate);
-  const klineMap = new Map();
-  etf50Klines.data.item.forEach((kline) => {
-    const date = dayjs(kline[0]).format('YYYY-MM-DD');
-    klineMap.set(date, {
-      open: kline[2],
-      close: kline[5],
-      low: kline[4],
-      high: kline[3],
-    });
-    return 0;
-  });
-  console.log(norhtbondResult);
-  const nDaysMap = new Map();
-  const valueArray = Array.from(norhtbondResult.values()).reverse();
-  valueArray.forEach((val, index) => {
-    const value1 = (((index + 1) <= valueArray.length - 1) ? valueArray[index + 1] : '0.0');
-    const value2 = (((index + 2) <= valueArray.length - 1) ? valueArray[index + 2] : '0.0');
-    const result = Number(val) + Number(value1) + Number(value2);
-    nDaysMap.set(dateArray[valueArray.length - index - 1], result.toFixed(2));
-    return result;
-  });
-  console.log(nDaysMap);
-};
-
-(async () => {
-  await strategy2();
-})();
-module.exports = { strategy1, strategy2 };
+// (async () => {
+//   const result = await strategy2('SH510500', 3, 100000, 0.5, 0.5);
+//   console.log(result);
+// })();
+module.exports = { strategy1 };
