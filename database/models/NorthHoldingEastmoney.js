7fafa0a810634da99b145fe78290fcec17a4b20f
leiweibo <leiweibo@gmail.com>
2021-08-28 23:59:17 +0800

    fetch data eastmoney north bonds related data

---
+++ database/models/NorthHoldingEastmoney.js
@@ -0,0 +1,79 @@
+const { Sequelize } = require('sequelize');
+const db = require('../db');
+
+const northHolding = db.define(
+  'north_holding_eastmoney',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      comment: 'id 自增长',
+    },
+    trade_date: {
+      type: Sequelize.DATE,
+    },
+    etfratio: {
+      type: Sequelize.DECIMAL(10, 4),
+    },
+    today_add_mkt_val: {
+      type: Sequelize.DECIMAL(32, 4),
+    },
+    today_add_mkt_ratio: {
+      type: Sequelize.DECIMAL(12, 10),
+    },
+    today_mkt_val: {
+      type: Sequelize.DECIMAL(32, 4),
+    },
+    today_mkt_ratio: {
+      type: Sequelize.DECIMAL(12, 4),
+    },
+    max_add_plate_by_mkt_val: {
+      type: Sequelize.STRING,
+    },
+    max_add_plate_by_ratio: {
+      type: Sequelize.STRING,
+    },
+    max_add_by_all_ratio: {
+      type: Sequelize.STRING,
+    },
+    max_add_by_mkt_val: {
+      type: Sequelize.STRING,
+    },
+    max_add_by_mkt_amt: {
+      type: Sequelize.STRING,
+    },
+    max_add_by_mkt_ratio: {
+      type: Sequelize.STRING,
+    },
+  },
+  {
+    tableName: 'north_holding_eastmoney',
+    createdAt: false,
+    updatedAt: false,
+    timestamps: false,
+  },
+);
+
+const normalizeArray = (dataArray) => {
+  if (dataArray) {
+    return dataArray
+      .map((obj) => {
+        const rObj = {};
+        rObj.trade_date = obj.HdDate;
+        rObj.etfratio = obj.Zdf;
+        rObj.today_add_mkt_val = obj.ShareSZ;
+        rObj.today_add_mkt_ratio = obj.ShareHoldAdd_ZB_ALL;
+        rObj.max_add_plate_by_mkt_val = obj.BKSName;
+        rObj.max_add_plate_by_ratio = obj.BKProportionName;
+        rObj.max_add_by_all_ratio = obj.BKMarketName;
+        rObj.max_add_by_mkt_val = obj.SZName_Max;
+        rObj.max_add_by_mkt_amt = obj.SharesName_Max;
+        rObj.max_add_by_mkt_ratio = obj.ProportionName_Max;
+        return rObj;
+      });
+  }
+  return dataArray;
+};
+
+module.exports = { northHolding, normalizeArray };
--- datafetcher/eastmoney_northolding.js
+++ datafetcher/eastmoney_northolding.js
@@ -3,20 +3,40 @@
  * 比如：某一日增幅最大的，未来5天的收益率怎么样。
  */
 const axios = require('axios');
+const winston = require('winston');
+const { northHolding, normalizeArray } = require('../database/models/NorthHoldingEastmoney');
 
 const generateUrl = function (ps) {
   return `https://dcfm.eastmoney.com/em_mutisvcexpandinterface/api/js/get?st=HdDate&sr=-1&ps=${ps}&p=1&type=HSGT20_SC_LS_MX&token=70f12f2f4f091e459a279469fe49eca5&js={"data":(x),"pages":(tp)}&filter=(Market='005')`
 };
 
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
+
+const blukInsertSecurity = async (dataArray) => {
+  try {
+    if (dataArray && dataArray.length > 0) {
+      await northHolding.bulkCreate(dataArray);
+      logger.info('bulk insert finished');
+    }
+  } catch (error) {
+    logger.error(error.stack);
+  }
+};
+
 const start = async () => {
-  const url = generateUrl(1);
+  const url = generateUrl(300);
   const resp = await axios.get(url);
   const { headers } = resp;
   const contentType = headers['content-type'];
   if (contentType && contentType.includes('text/plain')) {
     const result = JSON.parse(JSON.stringify(resp.data));
     const { data } = result;
-    console.log(data);
+    const finalResult = normalizeArray(data);
+    await blukInsertSecurity(finalResult);
   }
 };
 
